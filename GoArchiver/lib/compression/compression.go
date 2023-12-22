d6c53036c163d79186559a7ac873e84fcfbed29e
MrPositive <mrpositivekzn@gmail.com>
2023-12-22 17:34:05 +0300

    Initial commit

--- GoArchiver/lib/compression/compression.go
+++
@@ -1,9 +0,0 @@
-package compression
-
-type Encoder interface {
-	Encode(str string) []byte
-}
-
-type Decoder interface {
-	Decode(data []byte) string
-}
--- GoArchiver/main.vlc
+++
@@ -1,8 +0,0 @@
-package main
-
-import "GoArchiver/cmd"
-
-func main() {
-	//GoArchiver pack vlc <path> -out <path to>
-	cmd.Execute()
-}
